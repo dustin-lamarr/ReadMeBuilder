f24847acf4b8757951e93c95bef521b1d4ceba30
Dustin LaMarr <dustin.lamarr@gmail.com>
2020-11-12 08:49:21 -0700

    Added Markdown-It package. Created markdown sections

--- index.js
+++ index.js
@@ -1,57 +1,77 @@
 const inquirer = require('inquirer');
-// const projectTitle
-// const projectDesc
-// const installationInst
-// const usageInfo
-// const contribGuidelines
-// const testInst
-// const license
-// const githubUser
-// const emailAddress
+const fs = require('fs');
+const util = require('util');
+var MarkdownIt = require('markdown-it');
+const writeFileAsync = util.promisify(fs.writeFile);
 
-inquirer
-.prompt([
-    {
-        type: "input",
-        message: "Enter your full name:",
-        name: "name",
-    },
-    {
-        type: "input",
-        message: "Enter the name of your project:",
-        name: "projectname",
-    },
-    {
-        type: "input",
-        message: "Enter a brief description of your project:",
-        name: "description",
-    }
-    {
-        type: "input",
-        message: "Enter the installation instructions:",
-        name: "installation",
-    }
-    {
-        type: "input",
-        message: "Enter the usage instructions:",
-        name: "usage",
-    }
-    {
-        type: "input",
-        message: "Enter guidelines for contributing to the project:",
-        name: "contributors",
-    }
-    {
-        type: "input",
-        message: "Enter a brief description of your project:",
-        name: "description",
-    }
-    {
-        type: "input",
-        message: "Enter test instructions:",
-        name: "test",
-    }
-])
+const userInput = () =>
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter your full name:",
+            name: "owner",
+        },
+        {
+            type: "input",
+            message: "Enter the name of your project:",
+            name: "projectname",
+        },
+        {
+            type: "input",
+            message: "Enter a brief description of your project:",
+            name: "description",
+        },
+        {
+            type: "input",
+            message: "Enter the installation instructions:",
+            name: "installation",
+        },
+        {
+            type: "input",
+            message: "Enter the usage instructions:",
+            name: "usage",
+        },
+        {
+            type: "input",
+            message: "Enter guidelines for contributing to the project:",
+            name: "contributors",
+        },
+        {
+            type: "input",
+            message: "Enter a brief description of your project:",
+            name: "description",
+        },
+        {
+            type: "input",
+            message: "Enter test instructions:",
+            name: "test",
+        }
+    ]);
+
+const generateReadme = (answers) => {
+    md = new MarkdownIt(answers);
+    const projectOwner = md.render("# Project Owner");
+    const owner = md.renderInline(answers.owner);
+    const projectName = md.render('# Project Name');
+    const project = md.renderInline(answers.projectname);
+    const projectDesc = md.render("Project Description");
+    const description = md.render(answers.description);
+    const installationInst = md.render("# Installation Instructions");
+    const usageInfo
+    const contribGuidelines
+    const testInst
+    const license
+    const githubUser
+    const emailAddress
+    console.log(projectTitle);
+}
+userInput()
+    .then((answers) => writeFileAsync('ReadMe.md', generateReadme(answers)))
+
+
+
+// const generateReadme = (answers) =>
+
 
 
 // sections: Description, Table of Contents (links to sections), Installation, Usage, License, Contributing, Tests, and Questions
